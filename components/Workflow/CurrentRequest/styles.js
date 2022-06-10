628c86f1e466a5e053125525f46617cce6498dd1
seajensen <seajensen@gmail.com>
2022-06-09 23:02:46 -0600

    feat: added aries bifold patches
    Signed-off-by: seajensen <seajensen@gmail.com>

--- components/Workflow/CurrentRequest/styles.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import {StyleSheet} from 'react-native'
-
-const styles = StyleSheet.create({})
-
-export default styles
--- ios/AriesMobileAgent.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AriesMobileAgent.swift
-//  cardeaholder
-//
-//  Created by James Ebert on 5/3/21.
-//
-
-import Foundation
